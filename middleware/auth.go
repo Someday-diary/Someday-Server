f0e5bcbe083e9050c279e54f9db2c6699eba93a2
jangwoo kim <kjw2262@gmail.com>
2021-10-19 16:59:45 +0900

    [ADD] API key 유효성 인증 로직

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,7 +1,43 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"os"
 
-func Auth(c *gin.Context) {
+	"github.com/Someday-diary/Someday-Server/lib"
+	"github.com/Someday-diary/Someday-Server/model"
+	"github.com/gin-gonic/gin"
+)
 
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := c.GetHeader("access_token")
+
+		email, err := model.AccessTokenRedis.Get(context.Background(), t).Result()
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"msg": "세션이 만료되었습니다, 다시 로그인해주십쇼.",
+			})
+			c.Abort()
+			return
+		}
+
+		var secret model.Secret
+		model.DB.First(&secret, "email = ?", email)
+
+		c.Request.Header.Set("email", email)
+
+		ci, err := lib.NewNiceCrypto(os.Getenv("secret_key"), os.Getenv("cipher_iv_key"))
+		if err != nil {
+			_ = c.Error(err)
+		}
+		k, err := ci.Decrypt(secret.SecretKey)
+		if err != nil {
+			_ = c.Error(err)
+			c.Next()
+		}
+		c.Request.Header.Set("secret_key", k)
+		c.Next()
+	}
 }
