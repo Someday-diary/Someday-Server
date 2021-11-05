e4bf80ccb22db386235cce1e19fc365e9fd34d40
jangwoo kim <kjw2262@gmail.com>
2021-11-05 17:58:41 +0900

    [EDIT] 로그를 터미널에 찍지 않게 변경

--- model/database.go
+++ model/database.go
@@ -29,7 +29,7 @@ func Connect() {
 		os.Getenv("mysql_db_name"))
 
 	sqlDB, err := sql.Open("mysql", dsn)
-	db, err := gorm.Open(mysql.New(mysql.Config{Conn: sqlDB, DefaultStringSize: 191}), &gorm.Config{})
+	db, err := gorm.Open(mysql.New(mysql.Config{Conn: sqlDB, DefaultStringSize: 191}), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
 
 	if err != nil {
 		panic(err)
