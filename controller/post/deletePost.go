cf9271927c117e78361b6a1e4b3cdcd81cd063a8
jangwoo kim <kjw2262@gmail.com>
2021-11-05 17:59:32 +0900

    [EDIT] 포스트 삭제 로직 변경

--- controller/post/deletePost.go
+++ controller/post/deletePost.go
@@ -1,34 +1,38 @@
 package post
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/Someday-diary/Someday-Server/model"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func DeletePost() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var post model.Post
-		var n int64
-		err := model.DB.Delete(&post, "id = ?", c.Param("post_id")).Count(&n)
 
-		if err != nil {
+		postID := c.Param("post_id")
+		email := c.GetHeader("email")
+		err := model.DB.First(&post, "id = ?", postID).Error
+		if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 			panic(err)
-		}
-
-		if n == 0 {
+		} else if errors.Is(err, gorm.ErrRecordNotFound) {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"code": 110,
 			})
 			return
-		}
-
-		if email := c.GetHeader("email"); email != post.Email {
+		} else if post.Email != email {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"code": 111,
 			})
 			return
+		}
+
+		err = model.DB.Delete(&post, "id = ?", c.Param("post_id")).Error
+		if err != nil {
+			panic(err)
 		}
 
 		c.JSON(http.StatusOK, gin.H{
