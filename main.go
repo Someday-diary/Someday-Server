fafe83ab87ef48e7af69d6deb6dfc5015c27f40a
jangwoo kim <kjw2262@gmail.com>
2021-12-19 20:54:07 +0900

    [EDIT] 라우터 분리

--- main.go
+++ main.go
@@ -4,49 +4,23 @@ import (
 	"log"
 	"os"
 
-	"github.com/Someday-diary/Someday-Server/controller/post"
-	"github.com/Someday-diary/Someday-Server/controller/user"
 	"github.com/Someday-diary/Someday-Server/lib"
-	"github.com/Someday-diary/Someday-Server/middleware"
 	"github.com/Someday-diary/Someday-Server/model"
-	"github.com/gin-gonic/gin"
+	"github.com/Someday-diary/Someday-Server/router"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
+	if err := godotenv.Load(".env"); err != nil {
 		log.Fatalf("critical error! : %s", err.Error())
-
 	}
 
 	model.Connect()
 	lib.SystemCipher = lib.CreateCipher(os.Getenv("secret_key"))
 
-	r := gin.Default()
+	r := router.SetUpV1()
 
-	userAPI := r.Group("/user")
-	{
-		userAPI.POST("/sign_up", user.SignUp())
-		userAPI.POST("/login", user.Login())
-		userAPI.DELETE("/logout", middleware.Auth(), user.Logout())
-		userAPI.POST("/verify", user.SendEmail())
-		userAPI.POST("/verify/confirm", user.EmailConfirm())
-	}
-
-	postAPI := r.Group("/diary").Use(middleware.Auth())
-	{
-		postAPI.POST("", post.CreatePost())
-		postAPI.GET("", post.GetPost())
-		postAPI.GET("/month", post.GetPostByMonth())
-		postAPI.GET("/date", post.GetPostByDate())
-		postAPI.GET("/:post_id", post.GetPostByID())
-		postAPI.PATCH("/:post_id", post.EditPost())
-		postAPI.DELETE("/:post_id", post.DeletePost())
-	}
-
-	err = r.Run(":8080")
-	if err != nil {
+	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("critical error! : %s", err.Error())
 	}
 }
